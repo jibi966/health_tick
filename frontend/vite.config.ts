13072bda3b933670146807b217becace595bfdda
Jibin <jibinbabu966@gmail.com>
2025-03-14 21:34:26 +0530

    frontend build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,18 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: {
-        proxy: {
-            '': {
-                target: 'http://localhost:4000',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, ''),
-            },
-        },
-    },
-    build: {
-        outDir: '../backend/build',
-        emptyOutDir: true, // also necessary
-    }
+    // server: {
+    //     proxy: {
+    //         '': {
+    //             target: 'http://localhost:4000',
+    //             changeOrigin: true,
+    //             rewrite: (path) => path.replace(/^\/api/, ''),
+    //         },
+    //     },
+    // },
+    // build: {
+    //     outDir: '../backend/build',
+    //     emptyOutDir: true, // also necessary
+    // }
 })
 
